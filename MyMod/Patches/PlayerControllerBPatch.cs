3063ea4fdc7325aee0dc63f36c24e1dcd10004fe
Alexandr <alexandr.kirpichyov@gmail.com>
2024-01-14 00:41:15 +0200

    Initial commmit

---
+++ MyMod/Patches/PlayerControllerBPatch.cs
@@ -0,0 +1,29 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+
+namespace MyMod.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class PlayerControllerBPatch
+    {
+        //[HarmonyPatch(typeof(PlayerControllerB), "LateUpdate")]
+        [HarmonyPatch("LateUpdate")]
+        [HarmonyPostfix]
+        public static void InfiniteSprintPatchLate(ref float ___sprintMeter)
+        {
+            TutorialModBase.Log.LogInfo($"[Pre-Late] Current sprintMeter={___sprintMeter}");
+            ___sprintMeter = 1f;
+            TutorialModBase.Log.LogInfo($"[Post-Late] Current sprintMeter={___sprintMeter}");
+        }
+    
+        [HarmonyPatch("Update")]
+        //[HarmonyPatch(typeof(PlayerControllerB), "Update")]
+        [HarmonyPostfix]
+        public static void InfiniteSprintPatch(ref float ___sprintMeter)
+        {
+            TutorialModBase.Log.LogInfo($"[Pre] Current sprintMeter={___sprintMeter}");
+            ___sprintMeter = 1f;
+            TutorialModBase.Log.LogInfo($"[Post] Current sprintMeter={___sprintMeter}");
+        }
+    }
+}
---
+++ MyMod/TutorialModBase.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using GameNetcodeStuff;
+using HarmonyLib;
+
+namespace MyMod
+{
+    
+    [BepInPlugin(ModGuid, ModName, ModVersion)]
+    public class TutorialModBase : BaseUnityPlugin
+    {
+        private const string ModGuid = "Kirpichyov.MyMod";
+        private const string ModName = "Kirpichyov's MyMod";
+        private const string ModVersion = "1.0.0.0";
+
+        private readonly Harmony _harmony = new Harmony(ModGuid);
+        internal static ManualLogSource Log;
+    
+        public static TutorialModBase Instance { get; private set; }
+
+        private void Awake()
+        {
+            Instance = this;
+            Log = Logger;
+        
+            Log.LogInfo("MyMod loaded.");
+        
+            new Harmony(ModGuid).PatchAll(Assembly.GetExecutingAssembly());
+            _harmony.PatchAll(typeof(TutorialModBase));
+            _harmony.PatchAll(typeof(PlayerControllerB));
+        }
+    }
+}
