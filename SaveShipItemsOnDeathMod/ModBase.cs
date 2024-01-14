1305b118eced18eaceb3a5cc5702167dca13c28e
Alexandr <alexandr.kirpichyov@gmail.com>
2024-01-14 23:14:16 +0200

    Version 1

--- SaveShipItemsOnDeathMod/ModBase.cs
+++ SaveShipItemsOnDeathMod/ModBase.cs
@@ -8,16 +8,16 @@ namespace MyMod
 {
     
     [BepInPlugin(ModGuid, ModName, ModVersion)]
-    public class TutorialModBase : BaseUnityPlugin
+    public class ModBase : BaseUnityPlugin
     {
-        private const string ModGuid = "Kirpichyov.MyMod";
-        private const string ModName = "Kirpichyov's MyMod";
-        private const string ModVersion = "1.0.0.0";
+        private const string ModGuid = "Kirpichyov.SaveShipItemsOnDeath";
+        private const string ModName = "Kirpichyov's SaveShipItemsOnDeath";
+        private const string ModVersion = "1.0.0.1";
 
         private readonly Harmony _harmony = new Harmony(ModGuid);
         internal static ManualLogSource Log;
     
-        public static TutorialModBase Instance { get; private set; }
+        public static ModBase Instance { get; private set; }
 
         private void Awake()
         {
@@ -27,7 +27,7 @@ namespace MyMod
             Log.LogInfo("MyMod loaded.");
         
             new Harmony(ModGuid).PatchAll(Assembly.GetExecutingAssembly());
-            _harmony.PatchAll(typeof(TutorialModBase));
+            _harmony.PatchAll(typeof(ModBase));
             _harmony.PatchAll(typeof(PlayerControllerB));
         }
     }
---
+++ SaveShipItemsOnDeathMod/Patches/PlayerControllerBPatch.cs
@@ -0,0 +1,28 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using MyMod;
+
+namespace SaveShipItemsOnDeathMod.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class PlayerControllerBPatch
+    {
+        [HarmonyPatch(typeof(StartOfRound), "LateUpdate")]
+        [HarmonyPostfix]
+        public static void FrozeAllPlayersDead(StartOfRound __instance)
+        {
+            // TODO: Try to make loot in ship cost as half instead of penalty
+            if (__instance.allPlayersDead)
+            {
+                __instance.allPlayersDead = false;
+                ModBase.Log.LogInfo($"Was True, set to {__instance.allPlayersDead}");
+                
+                var terminal = UnityEngine.Object.FindObjectOfType<Terminal>();
+                var initialCredits = terminal.groupCredits;
+                terminal.groupCredits = 0;
+                ModBase.Log.LogInfo($"Credits reset. Was {initialCredits}");
+                HUDManager.Instance.DisplayTip("WARNING", "Credits were withdrawn due to crew death");
+            }
+        }
+    }
+}
